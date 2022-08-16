9a1d0b9219e6b1c10bf3779b10dc0fd8c897414a
Vishvesh <104484344+Vishveshpandit@users.noreply.github.com>
2022-08-17 01:53:05 +0530

    Add files via upload

---
+++ constants/update-data.js
@@ -0,0 +1,20 @@
+const UPDATE_DATA = [
+    {
+      imgSrc: "./images/profile-2.jpg",
+      profileName: "Jai Naryani",
+      message: "received his order of Night lion tech GPS drone.",
+      updatedTime: "2 Minutes Ago",
+    },
+    {
+      imgSrc: "./images/profile-3.jpg",
+      profileName: "Aashvi Shrivastav",
+      message: "declined her order of 2 DJI Air 2S.",
+      updatedTime: "5 Minutes Ago",
+    },
+    {
+      imgSrc: "./images/profile-4.jpg",
+      profileName: "Ananya Pandey",
+      message: "received his order of LARVENDER KF102 Drone.",
+      updatedTime: "6 Minutes Ago",
+    },
+  ];
